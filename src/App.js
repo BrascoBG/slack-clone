9c8326b121685119d5e71f38d6895bebe8ede601
Ivo Krastev <ivo.krustev@yahoo.com>
2020-10-20 13:52:28 +0300

    components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Slack Clone</h1>
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
